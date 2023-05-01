6da7dd455b4cb9b2e73c6fb6178361aee9927d7d
sayando <andousayuka@gmail.com>
2023-05-01 15:27:50 +0900

    add

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,7 +3,6 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,4 +13,5 @@ public class InputController {
         model.addAttribute("previous", previous);
         return "input";
     }
+
 }
